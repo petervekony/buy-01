45716979c7703bd50cd5a568205c1c9bed49f23e
Péter Vékony <peter.vekony@protonmail.com>
2023-09-16 13:36:54 +0300

    initial user service commit

---
+++ user-service/src/test/java/com/gritlab/buy01/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gritlab.buy01.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
