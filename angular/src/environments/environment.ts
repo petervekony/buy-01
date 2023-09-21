3e7300dc5a0d9633e3aede3150acc1b4f680290b
Péter Vékony <peter.vekony@protonmail.com>
2023-09-21 16:15:54 +0300

    fix environment ts files

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -1,14 +1,6 @@
 export const environment = {
   production: false,
-<<<<<<< Updated upstream
-  loginURL: 'http://localhost:8080/api/auth/signin',
-  signupURL: 'http://localhost:8080/api/auth/signup',
-  usersURL: 'http://localhost:8080/api/users',
-  productsURL: 'http://localhost:8080/api/products',
-  productOwnerURL: 'http://localhost:8080/api/products',
-=======
   loginURL: '/api/auth/signin',
   signupURL: '/api/auth/signup',
   usersURL: '/api/users',
->>>>>>> Stashed changes
 };
