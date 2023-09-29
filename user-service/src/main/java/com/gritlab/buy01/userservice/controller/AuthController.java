e0c67fa0901781a65677129a085f7211dc8b1189
Péter Vékony <peter.vekony@protonmail.com>
2023-09-29 12:34:57 +0300

    add signout

--- user-service/src/main/java/com/gritlab/buy01/userservice/controller/AuthController.java
+++ user-service/src/main/java/com/gritlab/buy01/userservice/controller/AuthController.java
@@ -1,7 +1,11 @@
 package com.gritlab.buy01.userservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,5 +32,23 @@ public class AuthController {
   @PostMapping("/signup")
   public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
     return authService.registerUser(signupRequest);
+  }
+
+  @PostMapping("/signout")
+  public ResponseEntity<HttpStatus> signOut(@CookieValue(name = "buy-01") String token) {
+    if (token == null) {
+      return new ResponseEntity<HttpStatus>(HttpStatus.FORBIDDEN);
+    }
+    ResponseCookie cookie =
+        ResponseCookie.from("buy-01", token)
+            .httpOnly(true)
+            .sameSite("Lax")
+            .domain("localhost")
+            .secure(true)
+            .path("/")
+            .maxAge(0)
+            .build();
+
+    return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body(null);
   }
 }
