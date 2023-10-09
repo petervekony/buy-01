88495ce1079b3e18598ff8982006aa3010bb3c6f
Péter Vékony <peter.vekony@protonmail.com>
2023-10-09 10:43:24 +0200

    check user/product on media upload

--- media-service/src/main/java/com/gritlab/buy01/mediaservice/controller/MediaController.java
+++ media-service/src/main/java/com/gritlab/buy01/mediaservice/controller/MediaController.java
@@ -1,12 +1,17 @@
 package com.gritlab.buy01.mediaservice.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,17 +21,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.gritlab.buy01.mediaservice.model.Media;
+import com.gritlab.buy01.mediaservice.model.Product;
 import com.gritlab.buy01.mediaservice.payload.response.MediaResponse;
+import com.gritlab.buy01.mediaservice.security.UserDetailsImpl;
 import com.gritlab.buy01.mediaservice.service.MediaService;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class MediaController {
   @Autowired MediaService mediaService;
+
+  @Autowired RestTemplate restTemplate;
 
   @PreAuthorize("isAuthenticated()")
   @GetMapping("/media/{id}")
@@ -53,8 +66,65 @@ public class MediaController {
   public ResponseEntity<?> createMedia(
       @RequestParam(required = false) String userId,
       @RequestParam(required = false) String productId,
-      @RequestPart MultipartFile image) {
-    return mediaService.createMedia(image, userId, productId);
+      @RequestPart MultipartFile image,
+      HttpServletRequest request) {
+
+    if (userId != null && productId != null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+    try {
+      Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+      UserDetailsImpl userDetails = null;
+
+      if (principal instanceof UserDetailsImpl) {
+        userDetails = (UserDetailsImpl) principal;
+      } else {
+        throw new Exception("Unexpected principal type");
+      }
+
+      if (productId != null) {
+        String cookie = getCookieValue(request, "buy-01");
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cookie", "buy-01=" + cookie);
+
+        URI uri = URI.create("http://product-service:8080/api/product/" + productId);
+
+        ResponseEntity<Product> response =
+            restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<>(headers), Product.class);
+
+        if (!response.getStatusCode().is2xxSuccessful()) {
+          return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+        Product product = response.getBody();
+
+        if (product.getUserId().equals(userDetails.getId())) {
+          return mediaService.createMedia(image, userId, productId);
+        }
+      }
+
+      if (userId != null) {
+        if (!userDetails.getId().equals(userId)) {
+          return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+        return mediaService.createMedia(image, userId, productId);
+      }
+    } catch (Exception e) {
+      System.out.println(e.toString());
+      return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+
+    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+  }
+
+  private String getCookieValue(HttpServletRequest request, String cookieName) {
+    if (request.getCookies() != null) {
+      for (Cookie cookie : request.getCookies()) {
+        if (cookie.getName().equals(cookieName)) {
+          return cookie.getValue();
+        }
+      }
+    }
+    return null;
   }
 
   @PreAuthorize("isAuthenticated()")
@@ -62,37 +132,4 @@ public class MediaController {
   public ResponseEntity<?> getProductThumbnail(@PathVariable("productId") String productId) {
     return mediaService.getProductThumbnail(productId);
   }
-  // @PreAuthorize("isAuthenticated()")
-  // @PostMapping("/products")
-  // public ResponseEntity<?> createProduct(@Valid @RequestBody ProductModel productModel) {
-  //   try {
-  //     Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-  //     String userId = null;
-
-  //     if (principal instanceof UserDetailsImpl) {
-  //       UserDetailsImpl userDetails = (UserDetailsImpl) principal;
-  //       userId = userDetails.getId();
-
-  //       // check if the user is ADMIN or SELLER to create product
-  //       if (!userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))
-  //           && !userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_SELLER")))
-  // {
-  //         ErrorMessage errorMessage =
-  //             new ErrorMessage(
-  //                 HttpStatus.FORBIDDEN.toString(),
-  //                 "Error: only admins and sellers can create products.");
-  //         return new ResponseEntity<>(errorMessage, HttpStatus.FORBIDDEN);
-  //       }
-  //     } else {
-  //       throw new Exception("Unexpected principal type");
-  //     }
-  //     productModel.setUserId(userId);
-
-  //     ProductModel _productModel = productService.createProduct(productModel);
-
-  //     return new ResponseEntity<>(_productModel, HttpStatus.CREATED);
-  //   } catch (Exception e) {
-  //     System.out.println(e.toString());
-  //     return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-  //   }
 }
