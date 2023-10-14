6e64aa13aa1a7b42616b15733c455023d36846f6
tvntvn13 <tnlmkhnn@me.com>
2023-10-14 22:45:17 +0300

    remove comments from home

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,11 +1,4 @@
-import {
-  // AfterViewInit,
-  Component,
-  // Renderer2,
-} from '@angular/core';
-import { Subscription } from 'rxjs';
-
-// import { FormStateService } from '../service/form-state.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -13,36 +6,6 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  filterSubscription: Subscription = Subscription.EMPTY;
-  constructor(
-    // private formStateService: FormStateService,
-    // private renderer: Renderer2,
-  ) {
+  constructor() {
   }
-  // ngAfterViewInit(): void {
-  //   console.log('this.container', this.container);
-  //   this.filterSubscription = this.formStateService.formOpen$.subscribe(
-  //     (isOpen) => {
-  //       if (isOpen) {
-  //         console.log(isOpen);
-  //         if (this.container) {
-  //           this.renderer.addClass(
-  //             this.container.nativeElement,
-  //             'blur-filter',
-  //           );
-  //           console.log(this.container.nativeElement.classList);
-  //         }
-  //       } else {
-  //         console.log(isOpen);
-  //         if (this.container) {
-  //           this.renderer.removeClass(
-  //             this.container.nativeElement,
-  //             'blur-filer',
-  //           );
-  //           console.log(this.container.nativeElement.classList);
-  //         }
-  //       }
-  //     },
-  //   );
-  // }
 }
