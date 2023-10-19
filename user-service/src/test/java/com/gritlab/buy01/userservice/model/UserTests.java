61d65c8a95e15e49687d856027f0e4b80ad4640e
Péter Vékony <peter.vekony@protonmail.com>
2023-10-19 15:37:36 +0300

    fix maven test types

--- user-service/src/test/java/com/gritlab/buy01/userservice/model/UserTests.java
+++ user-service/src/test/java/com/gritlab/buy01/userservice/model/UserTests.java
@@ -1,45 +1,50 @@
 package com.gritlab.buy01.userservice.model;
 
-import com.gritlab.buy01.userservice.model.enums.Role;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.gritlab.buy01.userservice.model.enums.Role;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+
 public class UserTests {
 
-    private Validator validator;
+  private Validator validator;
 
-    @BeforeEach
-    public void setUp() {
-        validator = Validation.buildDefaultValidatorFactory().getValidator();
-    }
+  @BeforeEach
+  public void setUp() {
+    validator = Validation.buildDefaultValidatorFactory().getValidator();
+  }
 
-    @Test
-    public void createUserWithValidFields() {
-        User user = new User("John Doe", "john.doe@example.com", "password123", Role.CLIENT);
-        Set<ConstraintViolation<User>> violations = validator.validate(user);
-        assertTrue(violations.isEmpty());
-    }
+  @Test
+  public void createUserWithValidFields() {
+    User user = new User("John Doe", "john.doe@example.com", "password123", Role.CLIENT);
+    Set<ConstraintViolation<User>> violations = validator.validate(user);
+    assertTrue(violations.isEmpty());
+  }
 
-    @Test
-    public void createUserWithShortName() {
-        User user = new User("Jo", "john.doe@example.com", "password123", Role.CLIENT);
-        Set<ConstraintViolation<User>> violations = validator.validate(user);
-        assertEquals(1, violations.size());
-        assertEquals("Error: Name has to be between 3 and 40 characters long", violations.iterator().next().getMessage());
-    }
+  @Test
+  public void createUserWithShortName() {
+    User user = new User("Jo", "john.doe@example.com", "password123", Role.CLIENT);
+    Set<ConstraintViolation<User>> violations = validator.validate(user);
+    assertEquals(1, violations.size());
+    assertEquals(
+        "Error: Name has to be between 3 and 40 characters long",
+        violations.iterator().next().getMessage());
+  }
 
-    @Test
-    public void createUsersWithCorrectRoles() {
-        User user1 = new User("Taneli", "taneli@taneli.com", "password123", Role.SELLER);
-        User user2 = new User("Taneli", "taneli@taneli.com", "password123", Role.CLIENT);
-        assertTrue(user1.getRole().equals(Role.SELLER));
-        assertTrue(user2.getRole().equals(Role.CLIENT));
-    }
+  @Test
+  public void createUsersWithCorrectRoles() {
+    User user1 = new User("Taneli", "taneli@taneli.com", "password123", Role.SELLER);
+    User user2 = new User("Taneli", "taneli@taneli.com", "password123", Role.CLIENT);
+    assertTrue(user1.getRole().equals(Role.SELLER));
+    assertTrue(user2.getRole().equals(Role.CLIENT));
+  }
 }
--- user-service/src/test/java/com/gritlab/buy01/userservice/service/AuthServiceTest.java
+++ user-service/src/test/java/com/gritlab/buy01/userservice/service/AuthServiceTest.java
@@ -1,82 +1,83 @@
 package com.gritlab.buy01.userservice.service;
 
-import com.gritlab.buy01.userservice.model.User;
-import com.gritlab.buy01.userservice.model.enums.Role;
-import com.gritlab.buy01.userservice.payload.request.LoginRequest;
-import com.gritlab.buy01.userservice.payload.request.SignupRequest;
-import com.gritlab.buy01.userservice.security.UserDetailsImpl;
-import com.gritlab.buy01.userservice.security.jwt.JwtUtils;
-import com.gritlab.buy01.userservice.repository.UserRepository;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Collections;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import com.gritlab.buy01.userservice.model.enums.Role;
+import com.gritlab.buy01.userservice.payload.request.LoginRequest;
+import com.gritlab.buy01.userservice.payload.request.SignupRequest;
+import com.gritlab.buy01.userservice.repository.UserRepository;
+import com.gritlab.buy01.userservice.security.UserDetailsImpl;
+import com.gritlab.buy01.userservice.security.jwt.JwtUtils;
 
 public class AuthServiceTest {
 
-    @Mock
-    private AuthenticationManager authenticationManager;
+  @Mock private AuthenticationManager authenticationManager;
 
-    @Mock
-    private UserRepository userRepository;
+  @Mock private UserRepository userRepository;
 
-    @Mock
-    private PasswordEncoder encoder;
+  @Mock private PasswordEncoder encoder;
 
-    @Mock
-    private JwtUtils jwtUtils;
+  @Mock private JwtUtils jwtUtils;
 
-    @Mock
-    private Authentication authentication;
+  @Mock private Authentication authentication;
 
-    @InjectMocks
-    private AuthService authService;
+  @InjectMocks private AuthService authService;
 
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
 
-    @Test
-    public void testAuthenticateUser() {
-        LoginRequest request = new LoginRequest();
-        request.setName("testName");
-        request.setPassword("testPassword");
+  @Test
+  public void testAuthenticateUser() {
+    LoginRequest request = new LoginRequest();
+    request.setName("testName");
+    request.setPassword("testPassword");
 
-        UserDetailsImpl userDetails = new UserDetailsImpl("1", "testName", "test@email.com", "testPassword", Collections.emptyList());
-        when(authentication.getPrincipal()).thenReturn(userDetails);
-        when(authenticationManager.authenticate(any())).thenReturn(authentication);
-        when(jwtUtils.generateTokenFromUserId("1")).thenReturn("testToken");
+    UserDetailsImpl userDetails =
+        new UserDetailsImpl(
+            "1", "testName", "test@email.com", "testPassword", Collections.emptyList());
+    when(authentication.getPrincipal()).thenReturn(userDetails);
+    when(authenticationManager.authenticate(any())).thenReturn(authentication);
+    when(jwtUtils.generateTokenFromUserId("1")).thenReturn("testToken");
 
-        var response = authService.authenticateUser(request);
+    var response = authService.authenticateUser(request);
 
-        assertNotNull(response);
-        assertTrue(response.getStatusCode().is2xxSuccessful());
-    }
+    assertNotNull(response);
+    assertTrue(response.getStatusCode().is2xxSuccessful());
+  }
 
-    @Test
-    public void testRegisterUser() {
-        SignupRequest request = new SignupRequest();
-        request.setName("testName");
-        request.setEmail("test@email.com");
-        request.setPassword("testPassword");
-        request.setRole(Role.CLIENT);
+  @Test
+  public void testRegisterUser() {
+    SignupRequest request = new SignupRequest();
+    request.setName("testName");
+    request.setEmail("test@email.com");
+    request.setPassword("testPassword");
+    request.setRole(Role.CLIENT);
 
-        when(userRepository.existsByName("testName")).thenReturn(false);
-        when(userRepository.existsByEmail("test@email.com")).thenReturn(false);
+    when(userRepository.existsByName("testName")).thenReturn(false);
+    when(userRepository.existsByEmail("test@email.com")).thenReturn(false);
 
-        var response = authService.registerUser(request);
+    ResponseEntity response = authService.registerUser(request);
 
-        assertNotNull(response);
-        assertEquals(201, response.getStatusCodeValue());
-    }
+    assertNotNull(response);
+    assertEquals(HttpStatus.CREATED, response.getStatusCode());
+  }
 }
