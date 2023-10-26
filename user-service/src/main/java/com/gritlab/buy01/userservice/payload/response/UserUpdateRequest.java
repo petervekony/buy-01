4157041e1d0e923df5a9772937e0fe80e016363b
tvntvn13 <tnlmkhnn@me.com>
2023-10-26 18:04:38 +0300

    add jacoco plugin for all backends
    - jacoco creates now reports after every test run
    - reports can be viewed from target/site/jacoco/

--- user-service/src/main/java/com/gritlab/buy01/userservice/payload/response/UserUpdateRequest.java
+++ user-service/src/main/java/com/gritlab/buy01/userservice/payload/response/UserUpdateRequest.java
@@ -1,23 +1,26 @@
 package com.gritlab.buy01.userservice.payload.response;
 
 import com.gritlab.buy01.userservice.model.enums.Role;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserUpdateRequest {
-    @Size(min=3, max=40, message="Error: Name has to be between 3 and 40 characters long")
-    private String name;
+  @Size(min = 3, max = 40, message = "Error: Name has to be between 3 and 40 characters long")
+  private String name;
 
-    @Email(message = "Invalid email format")
-    @Size(min=0, max=320, message = "Invalid email length")
-    private String email;
+  @Email(message = "Invalid email format")
+  @Size(min = 0, max = 320, message = "Invalid email length")
+  private String email;
 
-    private String password;
-    private Role role;
+  private String password;
+  private Role role;
 }
