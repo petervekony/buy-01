3025093527f638d3546a5cd59dcb50cc902a7fe4
Péter Vékony <peter.vekony@protonmail.com>
2023-11-23 12:23:34 +0200

    add sonarqube plugin to karma conf

--- angular/karma.conf.js
+++ angular/karma.conf.js
@@ -5,6 +5,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-sonarqube-unit-reporter'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('karma-junit-reporter'),
