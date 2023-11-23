721cda843e3626cdfa8a661d5a4d016a792db155
Péter Vékony <peter.vekony@protonmail.com>
2023-11-23 12:54:48 +0200

    try fixing angular karma config

--- angular/karma.conf.js
+++ angular/karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function (config) {
       require('karma-sonarqube-unit-reporter'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('karma-junit-reporter'),
+      // require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
