d0765db0144fe55da1d6442795369f483dcdc03f
Péter Vékony <peter.vekony@protonmail.com>
2023-11-27 14:25:54 +0200

    new script

--- run_sonar_scanner.sh
+++ run_sonar_scanner.sh
@@ -3,5 +3,14 @@
 # Move to the specified directory
 cd $1
 
+# Shift the first argument so that $@ contains only the sonar-scanner arguments
+shift
+
+# Run SonarScanner with all remaining arguments
+sonar-scanner "$@"
+
+# Move to the specified directory
+cd $1
+
 # Run SonarScanner with the provided arguments
 sonar-scanner -X $2
