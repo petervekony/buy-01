09da89088a840a552b84d91622291229f4f281d8
Péter Vékony <peter.vekony@protonmail.com>
2023-12-13 15:04:29 +0200

    add price and quantity validator to productdto

--- order-service/src/main/java/com/gritlab/buy01/orderservice/dto/ProductDTO.java
+++ order-service/src/main/java/com/gritlab/buy01/orderservice/dto/ProductDTO.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,12 +18,21 @@ import lombok.NoArgsConstructor;
 public class ProductDTO implements Serializable {
   private String id;
 
+  @NotNull
+  @Size(min = 3, max = 50, message = "Name has to be between 3 and 50 characters long")
   private String name;
 
+  @NotNull
+  @Size(min = 3, max = 300, message = "Description has to be between 3 and 300 characters long")
   private String description;
 
+  @NotNull
+  @DecimalMin(value = "0.0", message = "Price must be a positive value or zero")
+  @DecimalMax(value = "9999999999.0", message = "Maximum price is 9999999999")
   private Double price;
 
+  @NotNull
+  @Min(value = 0, message = "Quantity cannot be less than 0")
   private Integer quantity;
 
   private String userId;
--- product-service/src/main/java/com/gritlab/buy01/productservice/model/ProductDTO.java
+++ product-service/src/main/java/com/gritlab/buy01/productservice/model/ProductDTO.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -25,7 +27,8 @@ public class ProductDTO implements Serializable {
   private String description;
 
   @NotNull
-  @Min(value = 0, message = "Price must be a positive value or zero")
+  @DecimalMin(value = "0.0", message = "Price must be a positive value or zero")
+  @DecimalMax(value = "9999999999.0", message = "Maximum price is 9999999999")
   private Double price;
 
   @NotNull
@@ -38,6 +41,16 @@ public class ProductDTO implements Serializable {
 
   public ProductDTO(
       String name, String description, Double price, Integer quantity, String userId) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.quantity = quantity;
+    this.userId = userId;
+  }
+
+  public ProductDTO(
+      String id, String name, String description, Double price, Integer quantity, String userId) {
+    this.id = id;
     this.name = name;
     this.description = description;
     this.price = price;
